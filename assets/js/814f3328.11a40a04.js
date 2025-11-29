89819fd23b7396f06a0c0b41f5c56176a559edfd
KD Chang <kdchang.cc@gmail.com>
2025-11-29 11:49:51 +0800

    Deploy website - based on 71d4205a3f6d53e884c0c4e11315602fd57a6a28

---
+++ assets/js/814f3328.11a40a04.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkcareertags_github_io=globalThis.webpackChunkcareertags_github_io||[]).push([[472],{5513:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[]}')}}]);
--- assets/js/c15d9823.959c9eeb.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunkcareertags_github_io=globalThis.webpackChunkcareertags_github_io||[]).push([[908],{9328:a=>{a.exports=JSON.parse('{"metadata":{"permalink":"/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
