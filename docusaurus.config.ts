bd0ecefb769bb5163bd263a49213c9ff1af7b436
careertags <careertags.com@gmail.com>
2025-11-30 14:11:51 +0800

    feat: update config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "https://careertags.github.io",
+  url: "https://www.careertags.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
